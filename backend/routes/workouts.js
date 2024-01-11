3eae99d039825138eaf654d2702ccf37077776dd
RiYa368 <106838583+RiYa368@users.noreply.github.com>
2024-01-10 21:08:31 -0500

    added server responses on base URL post
    Updated post request on base URL by adding appropriate server response with data if applicable.

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,7 +1,5 @@
 const express = require('express')
-const {
-  createWorkout,
-} = require('../controllers/workoutController')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -10,13 +8,24 @@ router.get('/', (req, res) => {
   res.json({mssg: 'GET all workouts'})
 })
 
+
+
 // GET a single workout
 router.get('/:id', (req, res) => {
   res.json({mssg: 'GET a single workout'})
 })
 
 // POST a new workout
-router.post('/', createWorkout)
+router.post('/', async (req, res) => {
+const {title,load, reps} = req.body
+try {
+  const workout = await Workout.create({title, load, reps})
+  res.status(200).json(workout)
+}catch (error){
+  res.status(400).json({error: error.message})
+}
+})
+
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
