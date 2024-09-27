b912b1f58bf04278007f1d93fcae4612a7e1ce27
RiYa368 <106838583+RiYa368@users.noreply.github.com>
2024-09-27 15:03:38 -0400

    added code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,27 @@
 const user = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+
+// this mean that the user will logout automatically in 3 days
+const createToken = (_id) => {
+return jwt.sign({_id}, process.env.Secret, {expiresIn: '3d' })
+}
+
 
 //login user
 const loginUser = async (req,res) => {
-    res.json({mssg: 'login user'})
+    const {email,password} = req.body
+
+    try{
+        const user = await User.login(email, password)
+
+        //creating a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    }catch (error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 //signup user
@@ -11,7 +30,11 @@ const {email, password} = req.body
 
     try{
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+
+        //creating a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     }catch (error){
         res.status(400).json({error: error.message})
     }
